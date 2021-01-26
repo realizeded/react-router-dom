eb3c681f1447ea3f2b1a17ed73a88ed30d9bcdd5
lianjun <lianjun>
2021-01-26 11:02:59 +0800

    match->params

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter,Route,Link,Redirect,Switch} from './react-router-dom';
+import {HashRouter,Route,Link,Redirect,Switch} from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import User from './components/User';
